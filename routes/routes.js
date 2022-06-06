99293bb0b9977f9701a01dcdf736302d2c3b000b
abhishek1026 <abhishek@envoy.com>
2022-06-06 12:49:13 -0400

    adding /auth/resetPass PUT API endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -64,7 +64,7 @@ router.post('/create', async (req, res) => {
 router.post('/logout', isAuthenticated, async function(req, res) {
     var failed = false;
     var error = undefined;
-    var user = req.session.user;
+    const user = req.session.user;
     req.session.destroy(function(err) {
         if(err) {
             failed = true;
@@ -75,6 +75,50 @@ router.post('/logout', isAuthenticated, async function(req, res) {
         return res.status(500).json(error);
     }
     return res.status(200).send(`${user} logged out!`);
+});
+
+//PUT /resetPass
+router.put('/resetPass', async function(req, res) {
+    const newPass = req.body.newPass;
+    if(!newPass) {
+        return res.status(400).send("Invalid New Password!");
+    }
+    if (req.session && req.session.user) {
+        const username = req.session.user;
+        try {
+            const user = await User.findOne({username});
+            if(!user) {
+                return res.status(400).send(`Logged-in user (${username}) no longer exists in DB!`);
+            }
+            user.password = newPass;
+            const result = await user.save();
+            return res.status(200).json(result);
+        }
+        catch (error) {
+            return res.status(500).json({message: error.message});
+        }
+    }
+    const username = req.body.username;
+    const password = req.body.password;
+    if(!username || !password) {
+        return res.status(400).send("Invalid Request!");
+    }
+    try {
+        const user = await User.findOne({username});
+        if(!user) {
+            return res.status(401).send("Invalid Username!");
+        }
+        if(user.password != password) {
+            return res.status(401).send("Invalid Password!");
+        }
+        user.password = newPass;
+        const result = await user.save();
+        return res.status(200).json(result);
+    }
+    catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+    
 });
 
 module.exports = router;
